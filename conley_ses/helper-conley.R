b127f1cab9ddf4f7c625b75977bc203739f489da
Kyle F. Butts <buttskyle96@gmail.com>
2020-11-24 11:50:56 -0700

    Added no panel option for conley_ses

--- conley_ses/helper-conley.R
+++ conley_ses/helper-conley.R
@@ -3,15 +3,21 @@
 ## Kyle Butts, CU Boulder Economics 
 ## 
 ## This creates R functions for Conley (1999) Spatial-HAC standard errors. 
-## For panel or cross-sectional data. For cross-sectional just use this trick:
-## time = rep(1, nrow(X))
-## lag_cutoff = 1
+## For panel or cross-sectional data. For cross-sectional just don't include time and id vectors 
+## 
 ## -----------------------------------------------------------------------------
 
 library(Rcpp)
 library(RcppArmadillo)
 
 # setwd("")
+Rcpp::sourceCpp("helper-conley.cpp")
+library(tidyverse)
+library(Rcpp)
+library(RcppArmadillo)
+
+setwd("~/Documents/Projects/Spatial Spillover/")
+
 Rcpp::sourceCpp("helper-conley.cpp")
 
 
@@ -23,21 +29,25 @@ Rcpp::sourceCpp("helper-conley.cpp")
 #' @param X - nxk matrix of covariates (don't include lat and long if not in regression; if using fixed effects, they should not be included and X should be centered. See the `demeaned` option in `fixest::feols` if you want centered Xs)
 #' @param e - vector of residuals
 #' @param coords - nx2 matrix of lat and long (order doesn't matter)
-#' @param id - nx1 vector of id variable
-#' @param time - nx1 vector of time variable (can be in any units)
 #' @param dist_cutoff - Cutoff distance in km for spatial correlation
 #' @param lag_cutoff - Cutoff time in units of your time variable for serial correlation
 #' @param cores - multicore support with parallel::mclapply
 #' @param verbose - if TRUE, prints out a lot of details
+#' @param id - nx1 vector of id variable
+#' @param time - nx1 vector of time variable (can be in any units)
 #'
 #' @note could be speed up if you put the lapply within C++ code. I calculate the N^2 distances for each time period
-conley_ses <- function(X, e, coords, id, time, dist_cutoff, lag_cutoff, cores = 1, verbose = FALSE) {
+
+conley_ses <- function(X, e, coords, dist_cutoff, lag_cutoff, cores = 1, verbose = FALSE, id = NA, time = NA) {
 	
 	# Load multicore
 	if(cores > 1) library(parallel)
 	
 	n <- nrow(X)
 	k <- ncol(X)
+	
+	panel <- !(missing(time) & missing(id))
+	if(!panel) print("No Panel dimension provided. Using just Spatial adjustment")
 	
 	# Spatial Correlation ------------------------------------------------------
 	
@@ -50,22 +60,22 @@ conley_ses <- function(X, e, coords, id, time, dist_cutoff, lag_cutoff, cores = 
 		XeeXhs <- lapply(time_unique, function(t) {
 			sub_X <- X[time == t, ]
 			sub_e <- e[time == t]
-			sub_M <- M[time == t, ]
+			sub_coords <- coords[time == t, ]
 			n1 <- nrow(sub_X)
 			
-			XeeX_spatial(sub_M, dist_cutoff, sub_X, sub_e, n, k)
+			XeeX_spatial(sub_coords, dist_cutoff, sub_X, sub_e, n, k)
 		})
 	} else {
 		XeeXhs <- mclapply(time_unique, function(t) {
 			sub_X <- X[time == t, ]
 			sub_e <- e[time == t]
-			sub_M <- M[time == t, ]
+			sub_coords <- coords[time == t, ]
 			n1 <- nrow(sub_X)
 			
-			XeeX_spatial(sub_M, dist_cutoff, sub_X, sub_e, n, k)
+			XeeX_spatial(sub_coords, dist_cutoff, sub_X, sub_e, n, k)
 		}, mc.cores = cores)
 	}
-
+	
 	# Reduce across time
 	XeeX_spatial <- Reduce("+",  XeeXhs)
 	
@@ -73,7 +83,7 @@ conley_ses <- function(X, e, coords, id, time, dist_cutoff, lag_cutoff, cores = 
 	
 	# Serial Correlation -------------------------------------------------------
 	
-	XeeX_serial <- XeeX_serial(time, id, lag_cutoff, X, e, k)
+	if(panel) XeeX_serial <- XeeX_serial(time, id, lag_cutoff, X, e, k)
 	
 	
 	# Cov-Var Sandwich ---------------------------------------------------------
@@ -83,15 +93,18 @@ conley_ses <- function(X, e, coords, id, time, dist_cutoff, lag_cutoff, cores = 
 	
 	# Sandwiches w/ and w/o autocorrelation
 	V_spatial <- invXX %*% (XeeX_spatial) %*% invXX
-	V_spatial_HAC  <- invXX %*% (XeeX_spatial + XeeX_serial) %*% invXX
+	if(panel) V_spatial_HAC  <- invXX %*% (XeeX_spatial + XeeX_serial) %*% invXX
 	
 	
 	# Return Variance-Covariance Matrix
-	return(list(
-		"Spatial" = V_spatial,
-		"Spatial_HAC" = V_spatial_HAC
-	))
+	if(panel) {
+		return(list(
+			"Spatial" = V_spatial,
+			"Spatial_HAC" = V_spatial_HAC
+		))
+	} else {
+		return(list(
+			"Spatial" = V_spatial
+		))
+	}
 } 
-
-
-
